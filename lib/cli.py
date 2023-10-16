0e0ec54ede9593460f3b59f3bad808c24b5604d7
cb <christopher@bocki.com>
2023-10-16 02:33:45 +0200

    context: limit to 0..2

--- lib/cli.py
+++ lib/cli.py
@@ -70,7 +70,7 @@ def xmain():
     ap.add_argument('--ignore-case', '-i', action='store_true', help='ignore case distinctions')
     ap.add_argument('--word-regexp', '-w', action='store_true', help='match only whole words')
     ap.add_argument('--fixed-string', '-F', '-Q', action='store_true', help='interpret pattern as fixed string')
-    ap.add_argument('--context', '-C', metavar='N', default=2, type=int, help='print N lines of output context (default: 2)')
+    ap.add_argument('--context', '-C', metavar='N', default=2, type=int, choices=range(3), help='print N lines of output context (default: 2)')
     ap.add_argument('--color', choices=('never', 'always', 'auto'), default='auto', help='when to use colors (default: auto)')
     ap.add_argument('--web-browser', '-W', action='store_true', help='spawn a web browser')
     ap.add_argument('--delay', default=200, type=int, help='minimum time between requests, in ms (default: 200)')
@@ -84,10 +84,6 @@ def xmain():
               c, c++, objc, objc++, perl, python, go, java, ruby, shell, vala, javascript, json
     ''')
     options = ap.parse_args()
-    if options.context < 0:
-        options.context = 0
-    elif options.context > 2:
-        options.context = 2
     if (options.color == 'always') or (options.color == 'auto' and sys.stdout.isatty()):
         options.output = colors
     else:
